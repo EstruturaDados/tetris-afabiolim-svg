9128b1e16607409f39f35bcf6e5f52f1104ab8cd
afabiolim-svg <afabiolim@gmail.com>
2025-11-10 19:53:11 +0000

    varias tamaho

--- projetoTetris2.c
+++ projetoTetris2.c
@@ -11,3 +11,10 @@ typedef struct {
 
 #define TAM_FILA 5
 #define TAM_PILHA 3
+
+// Variáveis Tamanho (fila e pilha)
+Peca fila[TAM_FILA];
+int frente = 0, tamanhoFila = 0;
+Peca pilha[TAM_PILHA];
+int topo = -1;
+int proximoId = 1;
