d84f991e4f76a342f7d81114ad7f08284c7ced27
afabiolim-svg <afabiolim@gmail.com>
2025-11-10 19:04:02 +0000

    peças aleatoria

--- projetoTetris.c
+++ projetoTetris.c
@@ -17,3 +17,19 @@ Peca fila[TAM_FILA];
 int frente = 0;
 int tamanho = 0;
 int proximoId = 1;
+
+// Funçao auxiliares
+int isFull() { return tamanho == TAM_FILA; }
+int isEmpty() { return tamanho == 0; }
+
+
+// Peça aleatória
+Peca gerarPeca() {
+    char tipos[] = {'I','O','T','L','S','Z','J'};
+    int indice = rand() % 7;
+    Peca p;
+    p.id = proximoId++;
+    p.tipo = tipos[indice];
+    return p;
+}
+
