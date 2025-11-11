6bad1940ac732c44a592a64e50fc097db83abd9e
afabiolim-svg <afabiolim@gmail.com>
2025-11-11 18:59:48 +0000

    struct e tamanho#

--- projetoTetris3.c
+++ projetoTetris3.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct {
+    int id;
+    char tipo;
+} Peca;
+
+#define TAM_FILA 5
+#define TAM_PILHA 3
+#define TAM_HISTORICO 10
+
+
+
+
 int main() {
 
     return 0;
